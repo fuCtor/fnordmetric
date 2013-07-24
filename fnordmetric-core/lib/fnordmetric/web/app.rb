22727cd5ec171fcd364b8da69fe47aa7b8ad6f13
Alexey <ajieks@vmp.ru>
2013-07-24 22:07:41 +0600

    add warden support

--- fnordmetric-core/lib/fnordmetric/web/app.rb
+++ fnordmetric-core/lib/fnordmetric/web/app.rb
@@ -3,6 +3,7 @@
 class FnordMetric::App < Sinatra::Base
 
   include FnordMetric::AppHelpers
+  
 
   if RUBY_VERSION =~ /1.9.\d/
     Encoding.default_external = Encoding::UTF_8
@@ -11,6 +12,7 @@ class FnordMetric::App < Sinatra::Base
   if ENV['RACK_ENV'] == "test"
     set :raise_errors, true
   end
+   
 
   enable :session
   set :haml, :format => :html5
@@ -28,36 +30,54 @@ class FnordMetric::App < Sinatra::Base
   end
 
   def initialize(opts = {})
+  
     @opts = FnordMetric.options(opts)
-
+    
     @namespaces = FnordMetric.namespaces
     @redis = Redis.connect(:url => @opts[:redis_url])
-
+    
     super(nil)
   end
 
-  get '/' do
-    redirect "#{path_prefix}/#{@namespaces.keys.first}"
-  end
+    get "/" do
+      redirect "#{path_prefix}/#{@namespaces.keys.first}"
+    end
 
-  get '/:namespace' do
-    pass unless current_namespace
-    current_namespace.ready!(@redis)
-    haml :app
-  end
+    get "/:namespace" do
+      
+      if env['warden'].unauthenticated?
+        halt 401, "User is not logged in."      
+      end if env['warden']
+      
+      pass unless current_namespace
+      current_namespace.ready!(@redis)
+      haml :app
+    end
+    
+    get "/:namespace/" do
+    
+        if env['warden'].unauthenticated?
+        halt 401, "User is not logged in."      
+      end if env['warden']
+    
+      pass unless current_namespace
+      current_namespace.ready!(@redis)
+      haml :app
+    end
 
-  post '/events' do
-    params = JSON.parse(request.body.read) unless params
-    halt 400, 'please specify the event_type (_type)' unless params["_type"]
-    track_event((8**32).to_s(36), parse_params(params))
-  end
+    post "/events" do
+      params = JSON.parse(request.body.read) unless params
+      halt 400, 'please specify the event_type (_type)' unless params["_type"]
+      track_event((8**32).to_s(36), parse_params(params))
+    end
 
-  # FIXPAUL move to websockets
-  get '/:namespace/dashboard/:dashboard' do
-    dashboard = current_namespace.dashboards.fetch(params[:dashboard])
+    # FIXPAUL move to websockets
+    get "/:namespace/dashboard/:dashboard" do
+      dashboard = current_namespace.dashboards.fetch(params[:dashboard])
 
-    dashboard.to_json
-  end
+      dashboard.to_json
+    end
+
 
 end
 
